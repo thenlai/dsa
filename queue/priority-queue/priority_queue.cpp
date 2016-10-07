bd3d8d5b846822150de26bcad132458d67db2aa6
thenlai <tl_huang@outlook.com>
2016-10-07 20:31:06 +0800

    update: priority queue, update C++ code sample

--- queue/priority-queue/priority_queue.cpp
+++ queue/priority-queue/priority_queue.cpp
@@ -8,5 +8,22 @@ int main(void) {
 	q.push(1);
 	std::cout << q.top() << std::endl; //4
 
+
+	// 如果是指针，则按内存中的地址编号大小来比较优先级
+	std::priority_queue<int*> q2;
+	int a = 3;
+	q2.push(&a);
+	int b = 4;
+	q2.push(&b);
+	int c = 1;
+	q2.push(&c);
+	//std::cout << *(q2.top()) << std::endl; // 3
+
+	while(!q2.empty()) {
+		int *i = q2.top();
+		q2.pop();
+		std::cout << i << " = " << *i << std::endl;
+	}
+
 	return 0;
 }

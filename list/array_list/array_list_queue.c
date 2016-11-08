70ff86123299820674578080217efbf2db213eaf
thenlai <tl_huang@outlook.com>
2016-11-08 09:55:17 +0800

    update: array list

--- list/array_list/array_list_queue.c
+++ list/array_list/array_list_queue.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 const int INITIAL_SIZE = 20;
 const int STEP = 30;
@@ -8,9 +9,10 @@ int queue[INITIAL_SIZE];
 
 void push(int v) {
     if (count >= size) {
+        printf("\tresizing, %d, %d\n", count, size);
         // re-alloc memory
         size = count+STEP;
-        int *temp = malloc( size * sizeof int);
+        int *temp = malloc(size * sizeof(int));
         for (int i = 0; i < count; i++) {
             temp[i] = queue[i];
         }
@@ -20,6 +22,9 @@ void push(int v) {
 };
 
 int pop() {
+    if(count<=0) {
+        return -1;
+    }
     return queue[count--];
 };
 
@@ -28,5 +33,11 @@ int top() {
 };
 
 int main(void) {
-    // 
+    for(int i = 0; i < 80; i++) {
+        push(i%7);
+    }
+
+    for(int i = 79; i >= 0; i--) {
+        printf("pop: %d\n", pop());
+    }
 }
